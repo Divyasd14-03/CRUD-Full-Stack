5ecad51d05e9265de98a34e61c915fddf9d10706
Divyasd14-03 <divyadeshpande03@gmail.com>
2025-10-29 21:41:47 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors'
+import clientRoutes from './routes/clientRoute.js';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res) => {
+    res.send('<h1>Hello Backend</h1>');
+});
+
+app.use('/api',clientRoutes);
+
+app.listen(port, () => {
+    console.log("Listening on port 3000");
+})
+
